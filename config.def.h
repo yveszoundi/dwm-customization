ff748bf317d58f0b0fbe508166b5ae02ee8cfcda
Yves Zoundi <yveszoundi@gmail.com>
2021-12-18 14:10:26 -0500

    Update for constants

--- config.def.h
+++ config.def.h
@@ -7,12 +7,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:style=Regular:size=13" };
 static const char dmenufont[]       = "-*-terminus-bold-r-*-*-12-*-*-*-*-*-*-*";
-static const char normbordercolor[] = "#222222";
-static const char normbgcolor[]     = "#444444";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#eeeeee";
-static const char selfgcolor[]      = "#005577";
+static char normbordercolor[] = "#222222";
+static char normbgcolor[]     = "#444444";
+static char normfgcolor[]     = "#bbbbbb";
+static char selbordercolor[]  = "#005577";
+static char selbgcolor[]      = "#eeeeee";
+static char selfgcolor[]      = "#005577";
 
 static const char *colors[][3]      = {
 	/*               fg           bg               border         */
