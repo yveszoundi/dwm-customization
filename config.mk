0e24d6cd42364714a670d56266e1eec449afd495
Yves Zoundi <yveszoundi@gmail.com>
2021-12-19 17:06:07 -0500

    Compilation issues

--- config.mk
+++ config.mk
@@ -1,15 +1,3 @@
-# dwm version
-VERSION = 6.1
-
-# Customize below to fit your system
-
-# paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
-
-# compiler and linker
-CC = cc
-
 UNAME != uname
 
 include config.mk.${UNAME}
