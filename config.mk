dde786b367e27eb1d8b7b5cf88e8ca517ec31961
Yves Zoundi <yveszoundi@gmail.com>
2021-12-19 17:12:33 -0500

    Compilation issues

--- config.mk
+++ config.mk
@@ -1,3 +1,19 @@
+# dwm version
+VERSION = 6.1
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+# Linking
+FREETYPELIBS = -lfontconfig -lXft
+
+# compiler and linker
+CC = cc
+
 UNAME != uname
 
 include config.mk.${UNAME}
+
